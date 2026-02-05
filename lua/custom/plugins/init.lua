16f446391f20842073f2d463c6b97f9c42843166
citli <citlima29@gmail.com>
2026-02-05 19:30:37 +0000

    some changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,17 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+-- Default kickstart plugins
+-- NMAC427/guess-indent.nvim
+--lewis6991/gitsigns.nvim
+--folke/which-key.nvim
+--nvim-telescope/telescope.nvim
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'java',
   callback = function() require('custom.plugins.jdtls.jdtls_setup').setup {} end,
 })
-
+--require('lazy').setup {
+--{ 'nvim-treesitter/nvim-treesitter', branch = 'master', lazy = false, build = ':TSUpdate' },
+--}
 return {
   {
     'preservim/nerdtree',
     keys = function()
       return {
-        { '<leader>e', '<cmd>NERDTreeToggle<cr>', desc = 'Toggle File Tree' },
+        { '<leader>E', '<cmd>NERDTreeToggle<cr>', desc = 'Toggle File Tree' },
+        { '<leader>e', '<cmd>NERDTreeFocus<cr>', desc = 'Toggle File Tree' },
       }
     end,
   },
@@ -22,10 +30,10 @@ return {
       return {
         { '<leader>a', function() require('harpoon.mark').add_file() end, desc = 'Add file to Harpoon' },
         { '<leader>h', function() require('harpoon.ui').toggle_quick_menu() end, desc = 'Harpoon Toggle' },
-        { '<C-h>', function() require('harpoon.ui').nav_file(1) end, desc = 'Harpoon Toggle' },
-        { '<C-j>', function() require('harpoon.ui').nav_file(2) end, desc = 'Harpoon Toggle' },
-        { '<C-k>', function() require('harpoon.ui').nav_file(3) end, desc = 'Harpoon Toggle' },
-        { '<C-l>', function() require('harpoon.ui').nav_file(4) end, desc = 'Harpoon Toggle' },
+        { '<A-h>', function() require('harpoon.ui').nav_file(1) end, desc = 'Harpoon Toggle' },
+        { '<A-j>', function() require('harpoon.ui').nav_file(2) end, desc = 'Harpoon Toggle' },
+        { '<A-k>', function() require('harpoon.ui').nav_file(3) end, desc = 'Harpoon Toggle' },
+        { '<A-l>', function() require('harpoon.ui').nav_file(4) end, desc = 'Harpoon Toggle' },
       }
     end,
   },
