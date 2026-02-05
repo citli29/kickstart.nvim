eb7a97e7b63bdd273e9d37a2eba1bae29585a3df
citli <citlima29@gmail.com>
2026-02-05 14:04:29 +0000

    added Harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,7 +8,20 @@ return {
     'preservim/nerdtree',
     keys = function()
       return {
-        { '<leader>e', '<cmd>NERDTreeToggle<cr>' },
+        { '<leader>e', '<cmd>NERDTreeToggle<cr>', desc = 'Toggle File Tree' },
+      }
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',
+    keys = function()
+      return {
+        { '<leader>a', function() require('harpoon.mark').add_file() end, desc = 'Add file to Harpoon' },
+        { '<leader>h', function() require('harpoon.ui').toggle_quick_menu() end, desc = 'Harpoon Toggle' },
+        { '<C-h>', function() require('harpoon.ui').nav_file(1) end, desc = 'Harpoon Toggle' },
+        { '<C-j>', function() require('harpoon.ui').nav_file(2) end, desc = 'Harpoon Toggle' },
+        { '<C-k>', function() require('harpoon.ui').nav_file(3) end, desc = 'Harpoon Toggle' },
+        { '<C-l>', function() require('harpoon.ui').nav_file(4) end, desc = 'Harpoon Toggle' },
       }
     end,
   },
